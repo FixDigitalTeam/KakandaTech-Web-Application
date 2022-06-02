69c2f8e0dc193d29f32a974a1b223f693fc4051c
farulwananda <farulahmad2002@gmail.com>
2022-06-02 20:46:22 +0700

    update: view routing and url

--- resources/views/layouts/landing-main.blade.php
+++ resources/views/layouts/landing-main.blade.php
@@ -6,19 +6,19 @@
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <!-- Link of CSS files -->
-   <link rel="stylesheet" href="landing-assets/css/bootstrap.min.css">
-   <link rel="stylesheet" href="landing-assets/css/aos.css">
-   <link rel="stylesheet" href="landing-assets/css/all.min.css">
-   <link rel="stylesheet" href="landing-assets/css/odometer.min.css">
-   <link rel="stylesheet" href="landing-assets/css/remixicon.css">
-   <link rel="stylesheet" href="landing-assets/css/magnific-popup.min.css">
-   <link rel="stylesheet" href="landing-assets/css/meanmenu.min.css">
-   <link rel="stylesheet" href="landing-assets/css/swiper-bundle.min.css">
-   <link rel="stylesheet" href="landing-assets/css/owl.carousel.min.css">
-   <link rel="stylesheet" href="landing-assets/css/owl.theme.default.min.css">
-   <link rel="stylesheet" href="landing-assets/css/style.css">
+   <link rel="stylesheet" href="{{ url('landing-assets/css/bootstrap.min.css') }}">
+   <link rel="stylesheet" href="{{ url('landing-assets/css/aos.css') }}">
+   <link rel="stylesheet" href="{{ url('landing-assets/css/all.min.css') }}">
+   <link rel="stylesheet" href="{{ url('landing-assets/css/odometer.min.css') }}">
+   <link rel="stylesheet" href="{{ url('landing-assets/css/remixicon.css') }}">
+   <link rel="stylesheet" href="{{ url('landing-assets/css/magnific-popup.min.css') }}">
+   <link rel="stylesheet" href="{{ url('landing-assets/css/meanmenu.min.css') }}">
+   <link rel="stylesheet" href="{{ url('landing-assets/css/swiper-bundle.min.css') }}">
+   <link rel="stylesheet" href="{{ url('landing-assets/css/owl.carousel.min.css') }}">
+   <link rel="stylesheet" href="{{ url('landing-assets/css/owl.carousel.min.css') }}">
+   <link rel="stylesheet" href="{{ url('landing-assets/css/style.css') }}">
    <title>{{ $pagetitle }}</title>
-   <link rel="icon" type="image/png" href="landing-assets/img/favicon.png">
+   <link rel="icon" type="image/png" href="{{ url('landing-assets/img/favicon.png') }}">
 </head>
 
 <body>
@@ -28,7 +28,7 @@
          <div class="container">
             <div class="pakap-responsive-menu">
                <div class="logo">
-                  <a href="index-2"><img src="landing-assets/img/logo.png" alt="logo"></a>
+                  <a href="index-2"><img src="{{ url('landing-assets/img/logo.png') }}" alt="logo"></a>
                </div>
             </div>
          </div>
@@ -36,8 +36,8 @@
       <div class="pakap-nav">
          <div class="container">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-               <a class="navbar-brand" href="/">
-                  <img src="landing-assets/img/logo.png" alt="logo">
+               <a class="navbar-brand" href="{{ route('home') }}">
+                  <img src="{{ url('landing-assets/img/logo.png') }}" alt="logo">
                </a>
                <div class="collapse navbar-collapse mean-menu">
                   <ul class="navbar-nav">
@@ -82,7 +82,7 @@
       <div class="container">
          <div class="footer-content">
             <a href="#" class="logo">
-               <img src="landing-assets/img/logo.png" alt="logo">
+               <img src="{{ url('landing-assets/img/logo.png') }}" alt="logo">
             </a>
             <ul class="social-links">
                <li><a href="#" target="_blank"><i class="ri-facebook-fill"></i></a></li>
@@ -106,19 +106,19 @@
    <!-- End Footer Area -->
    <div class="go-top"><i class="ri-arrow-up-s-line"></i></div>
    <!-- Link of JS files -->
-   <script src="landing-assets/js/jquery.min.js"></script>
-   <script src="landing-assets/js/bootstrap.bundle.min.js"></script>
-   <script src="landing-assets/js/owl.carousel.min.js"></script>
-   <script src="landing-assets/js/swiper-bundle.min.js"></script>
-   <script src="landing-assets/js/magnific-popup.min.js"></script>
-   <script src="landing-assets/js/meanmenu.min.js"></script>
-   <script src="landing-assets/js/appear.min.js"></script>
-   <script src="landing-assets/js/odometer.min.js"></script>
-   <script src="landing-assets/js/form-validator.min.js"></script>
-   <script src="landing-assets/js/contact-form-script.js"></script>
-   <script src="landing-assets/js/ajaxchimp.min.js"></script>
-   <script src="landing-assets/js/aos.js"></script>
-   <script src="landing-assets/js/main.js"></script>
+   <script src="{{ url('landing-assets/js/jquery.min.js') }}"></script>
+   <script src="{{ url('landing-assets/js/bootstrap.bundle.min.js') }}"></script>
+   <script src="{{ url('landing-assets/js/owl.carousel.min.js') }}"></script>
+   <script src="{{ url('landing-assets/js/swiper-bundle.min.js') }}"></script>
+   <script src="{{ url('landing-assets/js/magnific-popup.min.js') }}"></script>
+   <script src="{{ url('landing-assets/js/meanmenu.min.js') }}"></script>
+   <script src="{{ url('landing-assets/js/appear.min.js') }}"></script>
+   <script src="{{ url('landing-assets/js/odometer.min.js') }}"></script>
+   <script src="{{ url('landing-assets/js/form-validator.min.js') }}"></script>
+   <script src="{{ url('landing-assets/js/contact-form-script.js') }}"></script>
+   <script src="{{ url('landing-assets/js/ajaxchimp.min.js') }}"></script>
+   <script src="{{ url('landing-assets/js/aos.js') }}"></script>
+   <script src="{{ url('landing-assets/js/main.js') }}"></script>
 </body>
 
 </html>
--- resources/views/pages/frontend/seo-content.blade.php
+++ resources/views/pages/frontend/seo-content.blade.php
@@ -1,133 +1,149 @@
 @extends('layouts.landing-main')
 @section('container')
-   <!-- Start Page Title Area -->
-   <div class="page-title-area">
-      <div class="container">
-         <div class="page-title-content">
-            <h2>Paket Pembuatan Konten SEO</h2>
-            <ul>
-               <li><a href="/">Home</a></li>
-               <li>Paket Pembuatan Konten SEO</li>
-            </ul>
-         </div>
+<!-- Start Page Title Area -->
+<div class="page-title-area">
+   <div class="container">
+      <div class="page-title-content">
+         <h2>Paket Pembuatan Konten SEO</h2>
+         <ul>
+            <li><a href="{{ route('home') }}">Home</a></li>
+            <li>Paket Pembuatan Konten SEO</li>
+         </ul>
       </div>
-      <div class="divider"></div>
-      <div class="lines">
-         <div class="line"></div>
-         <div class="line"></div>
-         <div class="line"></div>
-         <div class="line"></div>
-         <div class="line"></div>
-      </div>
-      <div class="banner-shape1"><img src="landing-assets/img/shape/shape9.png" alt="image"></div>
    </div>
-   <!-- End Page Title Area -->
+   <div class="divider"></div>
+   <div class="lines">
+      <div class="line"></div>
+      <div class="line"></div>
+      <div class="line"></div>
+      <div class="line"></div>
+      <div class="line"></div>
+   </div>
+   <div class="banner-shape1"><img src="{{ url('landing-assets/img/shape/shape9.png') }}" alt="image"></div>
+</div>
+<!-- End Page Title Area -->
 
-   <!-- Start Features Area -->
-   <div class="features-area pt-100">
-      <div class="container">
-         <div class="section-title">
-            <span class="sub-title">KEUNGGULAN KONTEN SEO KAMI</span>
-            <h2>Mengapa Harus Jasa Penulisan Artikel Kakanda Tech?</h2>
+<!-- Start Features Area -->
+<div class="features-area pt-100">
+   <div class="container">
+      <div class="section-title">
+         <span class="sub-title">KEUNGGULAN KONTEN SEO KAMI</span>
+         <h2>Mengapa Harus Jasa Penulisan Artikel Kakanda Tech?</h2>
+      </div>
+      <div class="row justify-content-center">
+         <div class="col-xl-4 col-lg-6 col-sm-6 col-md-6">
+            <div class="single-features-item">
+               <div class="icon">
+                  <i class="ri-eye-line"></i>
+               </div>
+               <h3>Human Friendly & SEO Friendly</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing. Mollitia nobis maxime ratione saepe eaque dolorem
+                  dolorum.</p>
+            </div>
          </div>
-         <div class="row justify-content-center">
-            <div class="col-xl-4 col-lg-6 col-sm-6 col-md-6">
-               <div class="single-features-item">
-                  <div class="icon">
-                     <i class="ri-eye-line"></i>
-                  </div>
-                  <h3>Human Friendly & SEO Friendly</h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing. Mollitia nobis maxime ratione saepe eaque dolorem dolorum.</p>
+         <div class="col-xl-4 col-lg-6 col-sm-6 col-md-6">
+            <div class="single-features-item">
+               <div class="icon bg2">
+                  <i class="ri-stack-line"></i>
                </div>
+               <h3>Harga Murah Kualitas Terjamin</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing. Mollitia nobis maxime ratione saepe eaque dolorem
+                  dolorum.</p>
             </div>
-            <div class="col-xl-4 col-lg-6 col-sm-6 col-md-6">
-               <div class="single-features-item">
-                  <div class="icon bg2">
-                     <i class="ri-stack-line"></i>
-                  </div>
-                  <h3>Harga Murah Kualitas Terjamin</h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing. Mollitia nobis maxime ratione saepe eaque dolorem dolorum.</p>
+         </div>
+         <div class="col-xl-4 col-lg-6 col-sm-6 col-md-6">
+            <div class="single-features-item">
+               <div class="icon bg3">
+                  <i class="ri-leaf-line"></i>
                </div>
+               <h3>Gratis Revisi Sepuasnya</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing. Mollitia nobis maxime ratione saepe eaque dolorem
+                  dolorum.</p>
             </div>
-            <div class="col-xl-4 col-lg-6 col-sm-6 col-md-6">
-               <div class="single-features-item">
-                  <div class="icon bg3">
-                     <i class="ri-leaf-line"></i>
-                  </div>
-                  <h3>Gratis Revisi Sepuasnya</h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing. Mollitia nobis maxime ratione saepe eaque dolorem dolorum.</p>
+         </div>
+         <div class="col-xl-4 col-lg-6 col-sm-6 col-md-6">
+            <div class="single-features-item">
+               <div class="icon bg4">
+                  <i class="ri-secure-payment-line"></i>
                </div>
+               <h3>Optimasi Keyword</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing. Mollitia nobis maxime ratione saepe eaque dolorem
+                  dolorum.</p>
             </div>
-            <div class="col-xl-4 col-lg-6 col-sm-6 col-md-6">
-               <div class="single-features-item">
-                  <div class="icon bg4">
-                     <i class="ri-secure-payment-line"></i>
-                  </div>
-                  <h3>Optimasi Keyword</h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing. Mollitia nobis maxime ratione saepe eaque dolorem dolorum.</p>
+         </div>
+         <div class="col-xl-4 col-lg-6 col-sm-6 col-md-6">
+            <div class="single-features-item">
+               <div class="icon bg5">
+                  <i class="ri-cloud-line"></i>
                </div>
+               <h3>Original, Tanpa Copas</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing. Mollitia nobis maxime ratione saepe eaque dolorem
+                  dolorum.</p>
             </div>
-            <div class="col-xl-4 col-lg-6 col-sm-6 col-md-6">
-               <div class="single-features-item">
-                  <div class="icon bg5">
-                     <i class="ri-cloud-line"></i>
-                  </div>
-                  <h3>Original, Tanpa Copas</h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing. Mollitia nobis maxime ratione saepe eaque dolorem dolorum.</p>
+         </div>
+         <div class="col-xl-4 col-lg-6 col-sm-6 col-md-6">
+            <div class="single-features-item">
+               <div class="icon bg6">
+                  <i class="ri-pie-chart-2-line"></i>
                </div>
-            </div>
-            <div class="col-xl-4 col-lg-6 col-sm-6 col-md-6">
-               <div class="single-features-item">
-                  <div class="icon bg6">
-                     <i class="ri-pie-chart-2-line"></i>
-                  </div>
-                  <h3>Konsultasi SEO</h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing. Mollitia nobis maxime ratione saepe eaque dolorem dolorum.</p>
-               </div>
+               <h3>Konsultasi SEO</h3>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing. Mollitia nobis maxime ratione saepe eaque dolorem
+                  dolorum.</p>
             </div>
          </div>
       </div>
    </div>
-   <!-- End Features Area -->
+</div>
+<!-- End Features Area -->
 
-   <!-- Start Pricing Area -->
-   <div class="pricing-area pt-100 pb-75">
-      <div class="container">
-         <div class="section-title">
-            <span class="sub-title">Paket Pembuatan Konten SEO</span>
-            <h2>Pilih Paket Berkualitas Terbaik Menurut Anda</h2>
-         </div>
-         <div class="row align-items-center justify-content-center">
-            @foreach ($data as $item)
-            <div class="col-lg-4 col-md-6 col-sm-6">
-               <div class="single-pricing-box">
-                     <div class="title">
-                        <h3>{{ $item->nama_package }}</h3>
-                        <p>{{ $item->deskripsi }}</p>
-                     </div>
-                     <div class="price">
-                        <span>Rp.</span>{{ $item->harga }}
-                     </div>
-                     @if ($maxtrx >= 4)
-                        <button class="default-btn" onclick="alert('Saat ini sedang tidak bisa melakukan pemesanan karena jumlah orderan melebihi batas. Silahkan melakukan pemesanan kembali pada tanggal …sekian…')">Pilih Paket</button>
-                     @else
-                        <a href="/trasanction-form" class="default-btn">Pilih Paket</a>
-                     @endif
-                     <ul class="features-list">
-                        <li><i class="{{ ($item->keuntungan_1 == '') ? '' : 'ri-check-line' }}"></i> {{ $item->keuntungan_1 }}</li>
-                        <li><i class="{{ ($item->keuntungan_2 == '') ? '' : 'ri-check-line' }}"></i> {{ $item->keuntungan_2 }}</li>
-                        <li><i class="{{ ($item->keuntungan_3 == '') ? '' : 'ri-check-line' }}"></i> {{ $item->keuntungan_3 }}</li>
-                        <li><i class="{{ ($item->keuntungan_4 == '') ? '' : 'ri-check-line' }}"></i> {{ $item->keuntungan_4 }}</li>
-                        <li><i class="{{ ($item->keuntungan_5 == '') ? '' : 'ri-check-line' }}"></i> {{ $item->keuntungan_5 }}</li>
-                        <li><i class="{{ ($item->keuntungan_6 == '') ? '' : 'ri-check-line' }}"></i> {{ $item->keuntungan_6 }}</li>
-                        <li><i class="{{ ($item->keuntungan_7 == '') ? '' : 'ri-check-line' }}"></i> {{ $item->keuntungan_7 }}</li>
-                        <li><i class="{{ ($item->keuntungan_8 == '') ? '' : 'ri-check-line' }}"></i> {{ $item->keuntungan_8 }}</li>
-                     </ul>
+<!-- Start Pricing Area -->
+<div class="pricing-area pt-100 pb-75">
+   <div class="container">
+      <div class="section-title">
+         <span class="sub-title">Paket Pembuatan Konten SEO</span>
+         <h2>Pilih Paket Berkualitas Terbaik Menurut Anda</h2>
+      </div>
+      <div class="row align-items-center justify-content-center">
+         @foreach ($data as $item)
+         <div class="col-lg-4 col-md-6 col-sm-6">
+            <div class="single-pricing-box">
+               <div class="title">
+                  <h3>{{ $item->nama_package }}</h3>
+                  <p>{!! $item->deskripsi !!}</p>
                </div>
+               <div class="price">
+                  <span>Rp.</span>{{ $item->harga }}
+               </div>
+               @if ($maxtrx >= 4)
+               <button class="default-btn"
+                  onclick="alert('Saat ini sedang tidak bisa melakukan pemesanan karena jumlah orderan melebihi batas. Silahkan melakukan pemesanan kembali pada tanggal …sekian…')">Pilih
+                  Paket</button>
+               @else
+               <a href="/trasanction-form" class="default-btn">Pilih Paket</a>
+               @endif
+               <ul class="features-list">
+                  <li><i class="{{ ($item->keuntungan_1 == '') ? '' : 'ri-check-line' }}"></i> {{ $item->keuntungan_1 }}
+                  </li>
+                  <li><i class="{{ ($item->keuntungan_2 == '') ? '' : 'ri-check-line' }}"></i> {{ $item->keuntungan_2 }}
+                  </li>
+                  <li><i class="{{ ($item->keuntungan_3 == '') ? '' : 'ri-check-line' }}"></i> {{ $item->keuntungan_3 }}
+                  </li>
+                  <li><i class="{{ ($item->keuntungan_4 == '') ? '' : 'ri-check-line' }}"></i> {{ $item->keuntungan_4 }}
+                  </li>
+                  <li><i class="{{ ($item->keuntungan_5 == '') ? '' : 'ri-check-line' }}"></i> {{ $item->keuntungan_5 }}
+                  </li>
+                  <li><i class="{{ ($item->keuntungan_6 == '') ? '' : 'ri-check-line' }}"></i> {{ $item->keuntungan_6 }}
+                  </li>
+                  <li><i class="{{ ($item->keuntungan_7 == '') ? '' : 'ri-check-line' }}"></i> {{ $item->keuntungan_7 }}
+                  </li>
+                  <li><i class="{{ ($item->keuntungan_8 == '') ? '' : 'ri-check-line' }}"></i> {{ $item->keuntungan_8 }}
+                  </li>
+               </ul>
             </div>
-            @endforeach
          </div>
+         @endforeach
       </div>
    </div>
-   <!-- End Pricing Area -->
+</div>
+<!-- End Pricing Area -->
 @endsection
