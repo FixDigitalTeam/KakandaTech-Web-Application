beebf9fa6612c91301497bc507cab1a5682c10a0
AHMAD WILDAN <ahmadwildan.smakensa@gmail.com>
2022-06-03 00:30:15 +0700

    update: menambahkan script untuk chat bot

--- resources/views/layouts/landing-main.blade.php
+++ resources/views/layouts/landing-main.blade.php
@@ -119,6 +119,18 @@
    <script src="{{ url('landing-assets/js/ajaxchimp.min.js') }}"></script>
    <script src="{{ url('landing-assets/js/aos.js') }}"></script>
    <script src="{{ url('landing-assets/js/main.js') }}"></script>
+   <!--Start of Tawk.to Script-->
+   <script type="text/javascript">
+      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+      (function(){
+      var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+      s1.async=true;
+      s1.src='https://embed.tawk.to/62965535b0d10b6f3e74fa46/1g4djpn61';
+      s1.charset='UTF-8';
+      s1.setAttribute('crossorigin','*');
+      s0.parentNode.insertBefore(s1,s0);
+      })();
+   </script>
+   <!--End of Tawk.to Script-->
 </body>
-
 </html>
