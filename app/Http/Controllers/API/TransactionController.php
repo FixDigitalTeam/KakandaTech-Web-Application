0bb6eccd02f2ba014b77dc7fcd606c3b42b835ea
farulwananda <farulahmad2002@gmail.com>
2022-06-02 12:12:20 +0700

    add: sample API for mobile

---
+++ app/Http/Controllers/API/TransactionController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class TransactionController extends Controller
+{
+    public function all(Request $request)
+    {
+        $id_transaction = $request->input('id_transaction');
+        $limit = $request->input('limit', 6);
+        $payment_status = $request->input('payment_status');
+
+        if($id_transaction)
+        {
+            $transaction = Transaction::with(['package', 'user'])->find($id_transaction);
+
+            if($transaction)
+            {
+                return ResponseFormatter::success(
+                    $transaction,
+                    'Data transaksi berhasil diambil'
+                );
+            }
+            else
+            {
+                return ResponseFormatter::error(
+                    null,
+                    'Data transaksi tidak ada',
+                    404
+                );
+            }
+        }
+
+        $transaction = Transaction::with(['package', 'user'])->where('user_id', Auth::user()->id);
+
+        if($payment_status)
+        {
+            $transaction->where('payment_status', $payment_status);
+        }
+
+        return ResponseFormatter::success(
+            $transaction->paginate($limit),
+            'Data list transaksi berhasil diambil'
+        );
+    }
+}
