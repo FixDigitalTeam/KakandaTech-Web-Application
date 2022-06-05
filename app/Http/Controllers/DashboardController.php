e36678ba157df60f9092175cddfaf2d0f732b668
farulwananda <farulahmad2002@gmail.com>
2022-06-05 11:26:25 +0700

    update: dashboard statistic

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,14 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaction;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
+        $customer = User::count();
+        $transaction_pending = Transaction::where('payment_status', 'Pending')->count();
+        $transaction_success = Transaction::where('payment_status', 'Success')->count();
+        $working_progress = Transaction::where('working_status', 'On Working')->count();
+        
+
         return view('pages.backend.dashboard.dashboard',[
-            'head' => 'Dashboard'
+            'head' => 'Dashboard',
+            'customer' => $customer,
+            'transaction_success' => $transaction_success,
+            'transaction_pending' => $transaction_pending,
+            'working_progress' => $working_progress
         ]);
     }
 }
--- resources/views/pages/backend/dashboard/dashboard.blade.php
+++ resources/views/pages/backend/dashboard/dashboard.blade.php
@@ -21,10 +21,27 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Customer</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">2</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $customer }}</div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-user fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Working Progress
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $working_progress }}</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
                         </div>
                     </div>
                 </div>
@@ -37,28 +54,11 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Order Success</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">2</div>
+                                Transaction Success</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $transaction_success }}</div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-clipboard-check fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-info shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Order Progress
-                            </div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">2</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
                         </div>
                     </div>
                 </div>
@@ -71,8 +71,8 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                Order Pending</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                                Transaction Pending</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $transaction_pending }}</div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-spinner fa-2x text-gray-300"></i>
