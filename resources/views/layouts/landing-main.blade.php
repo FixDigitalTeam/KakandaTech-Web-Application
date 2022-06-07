c9afdba5689fb0636b1b8f32a92876f64bb7b758
AHMAD WILDAN <ahmadwildan.smakensa@gmail.com>
2022-06-07 15:21:16 +0700

    update: mengubah link halaman contactus

--- resources/views/layouts/landing-main.blade.php
+++ resources/views/layouts/landing-main.blade.php
@@ -58,7 +58,7 @@
                      <li class="nav-item">
                         <a href="{{ route('blog') }}" class="nav-link">Blog</a>
                      </li>
-                     <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+                     <li class="nav-item"><a href="{{ route('contactus') }}" class="nav-link">Contact</a></li>
                   </ul>
                   <div class="others-option">
                      @auth
