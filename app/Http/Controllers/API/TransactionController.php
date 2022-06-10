bcf06fea8121eda23ba6a4db8695dad47385c04a
farulwananda <farulahmad2002@gmail.com>
2022-06-10 11:10:30 +0700

    update: API select by id_product

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -15,6 +15,8 @@ class TransactionController extends Controller
         $id_transaction = $request->input('id_transaction');
         $limit = $request->input('limit', 6);
         $payment_status = $request->input('payment_status');
+        $id_product = $request->input('id_product');
+
 
         if($id_transaction)
         {
@@ -39,12 +41,18 @@ class TransactionController extends Controller
 
         $transaction = Transaction::with(['package', 'user'])->where('user_id', Auth::user()->id);
 
+        if($id_product)
+        {
+            $transaction->where('id_product', $id_product);    
+        }
+
         if($payment_status)
         {
             $transaction->where('payment_status', $payment_status);
         }
 
         return ResponseFormatter::success(
+
             $transaction->paginate($limit),
             'Data list transaksi berhasil diambil'
         );
