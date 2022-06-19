8480b7e85f00c9eac240635269411ec930b707ce
AHMAD WILDAN <ahmadwildan.smakensa@gmail.com>
2022-06-19 17:20:32 +0700

    update: limit data review di landing home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
 {
     public function home() 
     {
-        $datareview = DB::table('reviews')->select('*')->join('transactions', 'transactions.id_transaction', '=', 'reviews.id_transaction')->join('users', 'users.id', '=', 'transactions.user_id')->join('packages', 'packages.id_package', '=', 'transactions.id_package')->join('products', 'products.id_product', '=', 'packages.id_product')->get();
+        $datareview = DB::table('reviews')->select('*')->join('transactions', 'transactions.id_transaction', '=', 'reviews.id_transaction')->join('users', 'users.id', '=', 'transactions.user_id')->join('packages', 'packages.id_package', '=', 'transactions.id_package')->join('products', 'products.id_product', '=', 'packages.id_product')->limit(4)->get();
         return view('pages.frontend.landing-home', [
             'pagetitle' => 'Kakanda Tech - Digital IT Agency Profesional, Berkualitas, dan Terpercaya',
             'datareview' => $datareview
