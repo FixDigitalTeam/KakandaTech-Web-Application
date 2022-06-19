13f5ea360a51d74ebe808782314f83fddb3f3087
AHMAD WILDAN <ahmadwildan.smakensa@gmail.com>
2022-06-19 14:02:21 +0700

    add: Model Table Reviews

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'id_review';
+
+    protected $fillable = [
+        'id_transaction', 'rate', 'review'
+    ];
 }
